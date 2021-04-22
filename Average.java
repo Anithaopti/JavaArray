6fc9b0a1d232c75fde43927944da08cd5b6c54aa
Anithaopti <81619901+Anithaopti@users.noreply.github.com>
2021-04-22 13:11:08 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,18 @@
+
+public class Average {
+	 public static void main(String arg[])	
+	 {
+	   int n=9,result=0;
+	 
+	   int a[]= {67,89,5689,67,54,456,45,78,967};
+	 
+	   
+	   for(int i=0;i<n;i++)
+	    
+		   result=result+a[i];
+	 	 	    
+	   System.out.println("average of a[] is ="+result/n);
+	 
+	 }
+	
+}
